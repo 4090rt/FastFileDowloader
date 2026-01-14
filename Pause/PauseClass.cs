4f03ea4f9049de469280577443ee4a4d44cf308e
4090rt <artem2007yannurow@gmail.com>
2026-01-14 20:20:28 +0300

    Add files via upload

---
+++ Pause/PauseClass.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDowloader.Pause
+{
+    public class PauseClass
+    {
+        private ManualResetEventSlim manualReset = new ManualResetEventSlim(true);
+        private readonly ILogger _logger;
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+        private CancellationToken _cancellationToken => _cts.Token;
+        public void Pause()
+        {
+            manualReset.Reset();
+            _logger.LogInformation("Загрузки проастановлены");
+        }
+
+        public void Resume()
+        { 
+            manualReset.Set();
+            _logger.LogInformation($"Загрузки возобнавлены");
+            
+        }
+
+        private async Task workerLoop()
+        {
+            while (!_cancellationToken.IsCancellationRequested)
+            {
+                manualReset.Wait(_cancellationToken);
+            }
+
+        }
+    }
+}
